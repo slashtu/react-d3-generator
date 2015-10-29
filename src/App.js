bc96b3de29ed6d8c6e08e56d7e0dc51bf0373d05
Slash <y12341234@gmail.com>
2015-10-29 17:02:06 +0800

    production build

--- src/App.js
+++ src/App.js
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
-import Bar from './Bar.js';
+import Line from './Chart/Line';
+import Bar from './Chart/Bar';
+import Area from './Chart/Area';
 
 var now = new Date().getTime();
 
-var dummyData = [];
+function createData(){
+  var dummyData = [];
 
-//dummy data
-for( let i=0; i < 50; i++ ){
-  let day = now - 86400000 * i ;
-  dummyData.unshift({ x: new Date(day), y: Math.random() * 100});
+  //dummy data
+  for( let i=0; i < 50; i++ ){
+    let day = now - 86400000 * i ;
+    dummyData.unshift({ x: new Date(day), y: Math.floor(Math.random() * 100)});
+  }
+
+  var chartData = [
+    { 
+      "name": "Series A",
+      "values": dummyData
+    }
+  ];
+
+  return chartData;
 }
 
-var chartData = [
-  { 
-    "name": "Series A",
-    "values": dummyData
-  }
-];
-
-var lineChartPosition = {
-  top: 220,
-  left: 65,
-  chartWidth: 441,
-};
 
 export class App extends Component {
+
   render() {
     return (
       <div>
-        <Bar data={chartData} 
-             heigh={200} 
+        <div style={{color: 'red'}}>"mousedown" left button, then "move"</div>
+        <Line data={createData()} 
+             fill={'#3182bd'}
+             title='Bar Chart'
+             xAxisTickInterval={{unit: 'day', interval: 10}}
+             height={200} 
              width={500} 
-             top={220} 
-             left={65} />
+             top={240} 
+             left={65} 
+        />
+        <Bar data={createData()} 
+             fill={'#3182bd'}
+             title='Bar Chart'
+             xAxisTickInterval={{unit: 'day', interval: 10}}
+             height={200} 
+             width={500} 
+             top={548} 
+             left={65} 
+        />
+        <Area data={createData()} 
+             fill={'#3182bd'}
+             title='Bar Chart'
+             xAxisTickInterval={{unit: 'day', interval: 10}}
+             height={200} 
+             width={500} 
+             top={850} 
+             left={65} 
+        />
       </div>
     );
   }
-
-
 }
---
+++ src/Chart/Line.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { BarChart, AreaChart, LineChart } from 'react-d3';
+
+export default class Line extends Component {
+
+  isMousedown = false;
+
+  state = {y:100, x:0};
+
+  static propTypes = { 
+    data: React.PropTypes.array.isRequired,
+    height: React.PropTypes.number,
+    width: React.PropTypes.number,
+    top: React.PropTypes.number,
+    left: React.PropTypes.number,
+    xAxisTickInterval: React.PropTypes.object,
+    yAxisScale: React.PropTypes.number,
+  };
+
+  static defaultProps = { 
+    yAxisScale: 1.5,
+  };
+
+  componentDidMount() {
+  }
+
+  move(event){
+
+    if(this.isMousedown == false) return;
+
+    var value = Math.round((this.props.top - event.pageY) / this.props.yAxisScale);
+
+    var pageX = event.pageX;
+
+    var index = this.getBarIndex(pageX);
+
+    this.setState({y: value, x: index});
+  }
+
+  getBarIndex( pageX ){
+
+    var dataLength = this.props.data[0].values.length;
+    var intervalWidth = ( this.props.width - 59 ) / dataLength;
+
+    return Math.floor((pageX - this.props.left) / intervalWidth) ;
+  }
+
+  mousedown(){
+  
+    this.isMousedown = true;
+  }
+
+  mouseup(){
+ 
+    this.isMousedown = false;
+  }
+
+  renderData(){
+
+    var str = "[ ";
+
+    for( let i=0; i< this.props.data[0].values.length; i++){
+      str += this.props.data[0].values[i].y + ' , ' ;
+    }
+
+    str = str.replace(/,\s*$/, "]");
+
+    return str;
+  }
+
+  render(){
+
+    if(this.props.data[0].values[this.state.x])
+      this.props.data[0].values[this.state.x].y = this.state.y;
+
+    return ( 
+      <div onMouseMove={this.move.bind(this)} onMouseDown={this.mousedown.bind(this)}  onMouseUp={this.mouseup.bind(this)}>
+        <LineChart
+                  data={this.props.data}
+                  width={this.props.width}
+                  height={this.props.height}
+                  fill={'#3182bd'}
+                  xAxisTickInterval={this.props.xAxisTickInterval}
+                  title='Line Chart'
+        />
+        {this.renderData()}
+      </div>
+    );
+  }
+}
